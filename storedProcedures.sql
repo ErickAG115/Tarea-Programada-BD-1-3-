32110ac7391c105dd0639fe9bb7d2510bece9f66
Daniel-TEC-17 <79490224+Daniel-TEC-17@users.noreply.github.com>
2022-04-06 23:33:57 -0600

    StoredProcedures

---
+++ storedProcedures.sql
@@ -0,0 +1,12 @@
+use [Tarea programada];
+
+--------------------------------------------------------------------------------------
+DROP PROCEDURE IF EXISTS filtrarNombre;
+
+CREATE PROCEDURE [dbo].[filtrarNombre]
+	AS BEGIN
+	SELECT [Puesto].[ID],[Puesto].[Nombre],[Puesto].[SalarioXHora] FROM [dbo].[Puesto] ORDER BY [Puesto].[Nombre];           
+	END
+GO
+	
+--------------------------------------------------------------------------------------
